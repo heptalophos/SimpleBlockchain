251f8b724bb94f1e37b54bef8c68487f7824dafe
Invigo <invigo.devel@gmail.com>
2020-02-21 16:53:44 +0200

    Revert "Update blockchain.py"
    This reverts commit 2cb0583abc2ef49c54cba4c01cbf57f31807d48d.

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -44,10 +44,12 @@ class Blockchain:
         
         while current_index < len(chain):
             block = chain[current_index]
+            # print("\n--------------------\n")
             # Check correctness of last block's hash
             if block['previous_hash'] != self.hash(last_block): 
                 return False
             # Check correctness of proof-of-work
+            # if not self.valid_proof(last_block['proof'], block['proof'], last_block['previous_hash']):
             if not self.valid_proof(last_block['proof'], block['proof'], block['previous_hash']):
                 return False
             last_block = block 
